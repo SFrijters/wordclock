10fca9f50c41685a0293babbf4bd3c649a7db80d
Stefan Frijters <sfrijters@gmail.com>
2025-04-25 23:02:20 +0200

    Add nix formatter / format

--- Wordclock/flake.nix
+++ Wordclock/flake.nix
@@ -29,67 +29,76 @@
           lib.attrsets.recursiveUpdate acc (injectSystem system value)
         ) { } (lib.genAttrs systems f);
     in
-      forSystems [ "x86_64-linux" "aarch64-linux" ] (
+    forSystems
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (
         system:
-      let
-        name = "Wordclock";
+        let
+          name = "Wordclock";
 
-        pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs { inherit system; };
 
-        python = pkgs.python3;
+          python = pkgs.python3;
 
-        pythonWithExtras = python.buildEnv.override {
-          extraLibs = [ ];
-        };
+          pythonWithExtras = python.buildEnv.override {
+            extraLibs = [ ];
+          };
 
-        # The variables starting with underscores are custom,
-        # the ones starting with ARDUINO are used by arduino-cli.
-        # See https://arduino.github.io/arduino-cli/0.33/configuration/ .
+          # The variables starting with underscores are custom,
+          # the ones starting with ARDUINO are used by arduino-cli.
+          # See https://arduino.github.io/arduino-cli/0.33/configuration/ .
 
-        # Store everything that arduino-cli downloads in a directory
-        # reserved for this project, and following the XDG specification,
-        # if the variable is available.
+          # Store everything that arduino-cli downloads in a directory
+          # reserved for this project, and following the XDG specification,
+          # if the variable is available.
 
-        # The _ARDUINO_PYTHON3 variable is passed to arduino-cli via the Makefile.
-        arduinoShellHookPaths = ''
-          if [ -z "''${_ARDUINO_PROJECT_DIR:-}" ]; then
-            if [ -n "''${_ARDUINO_ROOT_DIR:-}" ]; then
-              export _ARDUINO_PROJECT_DIR="''${_ARDUINO_ROOT_DIR}/${name}"
-            elif [ -n "''${XDG_CACHE_HOME:-}" ]; then
-              export _ARDUINO_PROJECT_DIR="''${XDG_CACHE_HOME}/arduino/${name}"
-            else
-              export _ARDUINO_PROJECT_DIR="''${HOME}/.arduino/${name}"
+          # The _ARDUINO_PYTHON3 variable is passed to arduino-cli via the Makefile.
+          arduinoShellHookPaths = ''
+            if [ -z "''${_ARDUINO_PROJECT_DIR:-}" ]; then
+              if [ -n "''${_ARDUINO_ROOT_DIR:-}" ]; then
+                export _ARDUINO_PROJECT_DIR="''${_ARDUINO_ROOT_DIR}/${name}"
+              elif [ -n "''${XDG_CACHE_HOME:-}" ]; then
+                export _ARDUINO_PROJECT_DIR="''${XDG_CACHE_HOME}/arduino/${name}"
+              else
+                export _ARDUINO_PROJECT_DIR="''${HOME}/.arduino/${name}"
+              fi
             fi
-          fi
-          # The variables below are respected by arduino-cli
-          export ARDUINO_DIRECTORIES_USER=$_ARDUINO_PROJECT_DIR
-          export ARDUINO_DIRECTORIES_DATA=$_ARDUINO_PROJECT_DIR
-          export ARDUINO_DIRECTORIES_DOWNLOADS=$_ARDUINO_PROJECT_DIR/staging
-          # This is used to override Python when invoking the Makefile
-          export _ARDUINO_PYTHON3=${python}
-        '';
-
-        devShellArduinoCLI = pkgs.mkShell {
-          name = "${name}-dev";
-          packages = with pkgs; [
-            arduino-cli  # For compiling and uploading the sketch
-            git          # For embedding a version hash into the sketch
-            gnumake      # To provide somewhat standardized commands to compile, upload, and monitor the sketch
-            picocom      # To monitor the serial output
-            pythonWithExtras  # So that the python3 wrapper of the esp8266 downloaded code can find a working python interpreter on the path
-            esptool
-            mkspiffs-presets.arduino-esp8266
-          ];
-          shellHook = ''
-            ${arduinoShellHookPaths}
-            2>&1 echo "==> Using arduino-cli version $(arduino-cli version)"
-            2>&1 echo "    Storing arduino-cli data for this project in ''${_ARDUINO_PROJECT_DIR}"
+            # The variables below are respected by arduino-cli
+            export ARDUINO_DIRECTORIES_USER=$_ARDUINO_PROJECT_DIR
+            export ARDUINO_DIRECTORIES_DATA=$_ARDUINO_PROJECT_DIR
+            export ARDUINO_DIRECTORIES_DOWNLOADS=$_ARDUINO_PROJECT_DIR/staging
+            # This is used to override Python when invoking the Makefile
+            export _ARDUINO_PYTHON3=${python}
           '';
-        };
 
-      in
+          devShellArduinoCLI = pkgs.mkShell {
+            name = "${name}-dev";
+            packages = with pkgs; [
+              arduino-cli # For compiling and uploading the sketch
+              git # For embedding a version hash into the sketch
+              gnumake # To provide somewhat standardized commands to compile, upload, and monitor the sketch
+              picocom # To monitor the serial output
+              pythonWithExtras # So that the python3 wrapper of the esp8266 downloaded code can find a working python interpreter on the path
+              esptool
+              mkspiffs-presets.arduino-esp8266
+            ];
+            shellHook = ''
+              ${arduinoShellHookPaths}
+              2>&1 echo "==> Using arduino-cli version $(arduino-cli version)"
+              2>&1 echo "    Storing arduino-cli data for this project in ''${_ARDUINO_PROJECT_DIR}"
+            '';
+          };
+
+        in
         {
           devShells.default = devShellArduinoCLI;
+
+          formatter =
+            lib.warnIf (builtins.hasAttr "nixfmt-tree" pkgs) "Replace nixfmt-rfc-style with nix-tree"
+              pkgs.nixfmt-rfc-style;
         }
-    );
+      );
 }
